cb32f3c5ea766c41bde0f3a2f816994db22b3ad8
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-07-23 17:43:14 -0700

    adding random user location/rotation

--- src/routes/DefendTheStick/socket/index.js
+++ src/routes/DefendTheStick/socket/index.js
@@ -9,6 +9,11 @@ AFRAME.registerComponent('player-emiter', {
   init () {
     this.id = this.data.id;
     this.isDefender = this.data.defender;
+    const newPosition = this.getRandomPosition();
+    const rotationTowardsCenter = this.calculateRotationTowardsCenter(newPosition);
+    this.el.setAttribute('position', newPosition); // position character randomly
+    this.el.setAttribute('rotation', rotationTowardsCenter); // Rotate character so it faces the center of the arena
+
     this.FireBase = {
       connection: Firebase,
       database: Firebase.database(),
@@ -38,5 +43,25 @@ AFRAME.registerComponent('player-emiter', {
   },
   onUserDisconnects () {
     this.FireBase.userRef.onDisconnect().remove();
+  },
+  getRandomPosition () {
+    const angle = Math.random() * Math.PI * 2;
+    const radius = 10; // size of the play-area
+    return {
+      x: Math.cos(angle) * radius,
+      z: Math.sin(angle) * radius,
+      y: 2,
+    };
+  },
+  calculateRotationTowardsCenter ({ x, z }) {
+    const angleRad = Math.atan((x / z));
+    const angleDeg = (angleRad * 180 / Math.PI);
+    const oldY = this.el.object3D.getWorldRotation()._y * 180 / Math.PI;
+    console.log(oldY, angleDeg, oldY + 90 + angleDeg)
+    return {
+      x: 0,
+      y: oldY + 90 + angleDeg,
+      z: 0,
+    }
   }
 });
