680e143ad0fdbba01469bcc3088ee60e501f20a0
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-07-23 18:08:10 -0700

    hotfix for display connected

--- src/routes/DefendTheStick/components/helpers.js
+++ src/routes/DefendTheStick/components/helpers.js
@@ -3,6 +3,7 @@ export const getDisplay = () => new Promise((resolve) => {
     const { capabilities } = displays[0];
     resolve(
       displays.length > 0 &&
+      capabilities &&
       displays[0].isConnected &&
       capabilities.hasExternalDisplay
     )
