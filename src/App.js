97cdeba0dafe5e5614d2532a860f99f74cf35a84
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2017-10-07 18:57:35 -0300

    Adding Vr Mobile Support with google's webvr-ui (#35)
    * Adding Vr Test
    
    * update
    
    * update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import 'aframe';
+import 'aframe-environment-component';
 import 'aframe-physics-system/index';
 import 'super-hands/dist/super-hands';
+import 'webvr-ui/build/webvr-ui';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import logo from './logo.svg';
