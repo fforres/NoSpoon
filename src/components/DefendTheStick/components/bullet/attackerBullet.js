71ec90ad23a73d54d32933d13dfddd486ca1f530
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2017-10-07 14:39:20 -0300

    Adding impulses to created bullets on defender side (#33)
    * adding impulses
    
    * adding second random bullet creator thingy
    
    * updating impulse values and cleanup;

--- src/components/DefendTheStick/components/bullet/attackerBullet.js
+++ src/components/DefendTheStick/components/bullet/attackerBullet.js
@@ -21,48 +21,22 @@ export default class AttackerBullet extends Component {
 
   componentWillUnmount() {
     if (this.functionReferenceToRemove) {
-      this.bullet.removeEventListener('body-loaded', () => {
-        this.functionReferenceToRemove = arguments.calee;
-      })
+      this.bullet.removeEventListener('body-loaded');
+      this.bullet.parentNode.removeChild(this.bullet);
     }
   }
 
   bodyLoaded() {
-    const impulseAmount = 20;
-    // Use an origin point behind the head, not at the head, so
-    // there's a useful vector between the origin and the projectile.
+    const impulseAmount = 10;
     const directionVector = new CANNON.Vec3().copy(
       this.worldOrigin.vsub(this.bullet.body.position)
     );
     const bulletVector = new CANNON.Vec3();
     bulletVector.copy(this.bullet.body.position);
 
-    // const force = pStart.vsub(this.bullet.body.position);
     directionVector.normalize();
     directionVector.scale(impulseAmount, directionVector);
-
     this.bullet.body.applyImpulse(directionVector, bulletVector);
-
-    // const worldOrigin = new CANNON.Vec3(0, 0, 0);
-    // let directionVector = new CANNON.Vec3();
-    // directionVector = directionVector.copy(worldOrigin.vsub(this.bullet.body.position));
-
-    // Can't apply forces during the same tick that attaches the body, because
-    // it hasn't been fully synced to the physics sim. (bug)
-    // if (this.bullet) {
-    //   const pointOfOrigin = new CANNON.Vec3(0, 0, 0);
-    //   const bulletPosition = this.bullet.object3D.getWorldPosition();
-    //   const force = new CANNON.Vec3().copy(bulletPosition);
-    //   // const force = this.bullet.body.position;
-    //   // force.normalize();
-    //   // force.scale(impulseAmount, force);
-    //   this.bullet.body.applyImpulse(
-    //     // new CANNON.Vec3().copy(new CANNON.Vec3(0, -1, -1)),
-    //     new CANNON.Vec3(0, 1, -1),
-    //     new CANNON.Vec3().copy(this.bullet.object3D.getWorldPosition()),
-    //   );
-    // }
-
   }
   render() {
     const { name, position } = this.props;
